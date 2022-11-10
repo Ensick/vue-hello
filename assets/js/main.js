4e59aa7a2bd3b4183541187c33efb3b05362b05d
Ensick <fede.censi96@gmail.com>
2022-11-10 14:31:07 +0100

    Fine Esercizio + Bonus

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+Descrizione:
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+var app = new Vue({
+    el : "#root",
+    data :{
+        messaggio: "Sono un messaggio all'interno di un h1",
+        immagine: "./assets/img/photo-1666294715367-5a01360e30a5.avif"
+    }
+})
+
